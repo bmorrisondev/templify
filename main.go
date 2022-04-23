2b5f3a31a53e149cbb8c0c049e51196ee9a389a6
Okabe Rintarou <eliotandelon@gmail.com>
2022-04-22 23:52:26 -0500

    add validation for flag branch

--- main.go
+++ main.go
@@ -103,6 +103,19 @@ type GitHubResponse struct {
 	DefaultBranch string `json:"default_branch"`
 }
 
+func VerifyBranchName(username string, reponame string, branchName string) bool {
+	response, err := http.Get("https://api.github.com/repos/" + username + "/" + reponame + "/branches" + branchName)
+	if err != nil {
+		check(err)
+	}
+
+	if response.StatusCode == http.StatusNotFound {
+		return false
+	}
+
+	return true
+}
+
 func GetMainBranchName(username string, reponame string) (string, error) {
 	response, err := http.Get("https://api.github.com/repos/" + username + "/" + reponame)
 	if err != nil {
@@ -181,7 +194,11 @@ func main() {
 		check(err)
 		branchName = mainBranchName
 	} else {
-		// TODO: Check if the branch exists here
+		existsbranch := VerifyBranchName(username, reponame, *branchNamePtr)
+		if !existsbranch {
+			fmt.Printf("The branch %v not exists for the repository %v\n", *branchNamePtr, repoUrl)
+			os.Exit(1)
+		}
 		branchName = *branchNamePtr
 	}
 
